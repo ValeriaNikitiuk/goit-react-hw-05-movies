70ef43f6de36f7dece05fea3e8af2f7ad63c2ec9
ValeriaNikitiuk <108237625+ValeriaNikitiuk@users.noreply.github.com>
2023-02-09 21:20:25 +0000

    Deploying to gh-pages from @ ValeriaNikitiuk/goit-react-hw-05-movies@8bf96b1a95b689830a7f3282471a61acf85d6897 🚀

---
+++ static/js/533.c00d2791.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[533],{533:function(e,n,s){s.r(n),s.d(n,{SharedLayout:function(){return u}});var r=s(791),t=s(689),i={},c=s(87),a=s(184),o=function(){return(0,a.jsxs)("nav",{className:i.navList,children:[(0,a.jsx)(c.OL,{to:"/",children:"Home"}),(0,a.jsx)(c.OL,{to:"/movies",children:"Movies"})]})},u=function(){return(0,a.jsxs)(a.Fragment,{children:[(0,a.jsx)(o,{}),(0,a.jsx)(r.Suspense,{fallback:null,children:(0,a.jsx)(t.j3,{})})]})}}}]);
+//# sourceMappingURL=533.c00d2791.chunk.js.map
